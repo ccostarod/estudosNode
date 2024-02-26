d791794ddd30db3f787331ac2aee379fdf971775
ccostarod <ccostarodrigoo@gmail.com>
2024-02-26 19:22:11 -0300

    Estabelecendo parametros de rota

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express(); //Recebe o express e cria uma copia do express para a variavel var, agora trata-se da variavel principal.
 
 app.get("/", function(req, res){ //Definiu uma rota
-    res.send("Seja bem-vindo ao app")
+    res.send("Seja bem-vindo ao app do rodrigo")
 });
 
 app.get("/sobre", function(req, res){
@@ -12,6 +12,10 @@ app.get("/sobre", function(req, res){
 app.get("/blog", function(req, res){
     res.send("Bem-vindo ao blog");
 });
+//Nessa rota de baixo sera estabelecido um parametro atraves da "/:"
+app.get("/ola/:nome/:cargo", function(req, res){ 
+    res.send("ola " + req.params.nome + " sua profissao é " + req.params.cargo); //aqui, por meio do parametro da function request, eu consegui pegar o dado contido dentro do parametro nome definido na rota.
+})
 
 app.listen(8081, function(){
     console.log("Servidor Rodando na url http://localhost:8081")
