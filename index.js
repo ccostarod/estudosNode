bbca637311a84a2e7616a5357cf4345c959ef141
ccostarod <ccostarodrigoo@gmail.com>
2024-02-27 16:19:39 -0300

    Adicionando dados ao banco de dados e listando eles na rota '/'

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const express = require("express");
 const app = express(); //Recebe o express e cria uma copia do express para a variavel var, agora trata-se da variavel principal.
-const Sequelize = require('sequelize');
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
+const Post = require("./models/Post")
 
 // Config
-    // Template Engine
-    //nas duas linhas abaixo estamos falando para o express que vamos usar o handlebars como template engine.
+    // Template Engine (nas duas linhas abaixo estamos falando para o express que vamos usar o handlebars como template engine.)
         app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
-    // Conexão com o banco de dados
-        const sequelize = new Sequelize('test', 'root', 'Welcome21090400.123', 
-        {host: 'localhost', dialect: 'mysql'});
     //body parser
         app.use(bodyParser.urlencoded({extended: false}))
         app.use(bodyParser.json())
+
+    //handlebars
+        app.engine('handlebars', handlebars.engine({
+        defaultLayout: 'main',
+        runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+            allowProtoMethodsByDefault: true,
+        },
+        }))
+        app.set('view engine', 'handlebars');
+
 // Rotas
-    app.get('/cad', function(req, res){
+    app.get('/cad', function(req, res){ 
         res.render('formulario')
+    })
+
+    app.get('/', function(req, res){ //Listagem de posts criados
+        Post.findAll({order: [['createdAt','DESC']]}).then(function(posts){
+            res.render('home', {posts: posts})
+        })
     })
     //rota post (nao acessavel por meio da URL):
         app.post('/add', function(req, res){ //nao utilizamos app.get por conta de tratar-se de uma req de method='POST' no formulario.
-            res.send('texto: ' + req.body.titulo + " Conteudo: " + req.body.conteudo);
-        })
+            Post.create({
+                titulo: req.body.titulo,
+                conteudo: req.body.conteudo
+            }).then(function(){
+                res.redirect('/');
+            }).catch(function(erro){
+                res.send("Houve um erro: " + erro);
+            });
+        });
 
-app.listen(8081, function(){
-    console.log("Servidor Rodando na url http://localhost:8081")
-}); //Iniciar servidor, e deve ser a ultima linha do codigo
+//Iniciar servidor, e deve ser a ultima linha do codigo
+    app.listen(8082, function(){
+        console.log("Servidor Rodando na url http://localhost:8082")
+    }); 
---
+++ models/db.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postapp', 'root', 'Welcome21090400.123', {
+    host: 'localhost', 
+    dialect: 'mysql'
+});
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
