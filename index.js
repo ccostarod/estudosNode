80dbbded0e4a6a14b3a957fcf63f5cc83c89b944
ccostarod <ccostarodrigoo@gmail.com>
2024-02-27 00:58:17 -0300

    Projeto formulario: Implementação handlebars e rota do tipo post

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express(); //Recebe o express e cria uma copia do express para a variavel var, agora trata-se da variavel principal.
+const Sequelize = require('sequelize');
+const handlebars = require("express-handlebars");
+
+
+// Config
+    // Template Engine
+    //nas duas linhas abaixo estamos falando para o express que vamos usar o handlebars como template engine.
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexão com o banco de dados
+        const sequelize = new Sequelize('test', 'root', 'Welcome21090400.123', 
+        {host: 'localhost', dialect: 'mysql'});
+// Rotas
+    app.get('/cad', function(req, res){
+        res.render('formulario')
+    })
+    //rota post (nao acessavel por meio da URL):
+        app.post('/add', function(req, res){ //nao utilizamos app.get por conta de tratar-se de uma req de method='POST' no formulario.
+            res.send('Formulario Recebido');
+        })
+
+app.listen(8081, function(){
+    console.log("Servidor Rodando na url http://localhost:8081")
+}); //Iniciar servidor, e deve ser a ultima linha do codigo
