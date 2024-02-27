92b9c9683495db1af4f2bac2c2884a6d2497fbef
ccostarod <ccostarodrigoo@gmail.com>
2024-02-26 21:57:40 -0300

    implementig sequelize

---
+++ test.js
@@ -0,0 +1,3 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('test', 'root', 'Welcome21090400.123', {host: 'localhost', dialect: 'mysql'});
+
---
+++ teste.sql
@@ -0,0 +1,18 @@
+CREATE TABLE usuarios (
+  nome VARCHAR(50),
+  email VARCHAR(100),
+  idade INT
+); // Criação de tabela
+
+INSERT INTO usuarios (nome, email, idade)
+VALUES ('Rodrigo Costa', 'rodrigo@example.com', 19); // Inserção de dados
+
+
+SELECT FROM * usuarios; // Seleciona todos os usuários (Consulta de dados)
+SELECT FROM * usuarios WHERE idade > 18; // Seleciona todos os usuários com idade maior que 18 (Consulta de dados)
+
+DELETE FROM usuarios WHERE nome = 'Rodrigo Costa'; // Deleta o usuário (Remoção de dados)
+
+UPDATE usuarios SET idade = 20 WHERE nome = 'Rodrigo Costa'; // Atualiza a idade do usuário (Atualização de dados)
+
+
